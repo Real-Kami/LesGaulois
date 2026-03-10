24c70e08be02d4737672c8915db8c74a8ff45939
Camille Golfin <glc5252a@ensfsi.onmicrosoft.com>
2026-03-10 19:37:02 +0100

    Ajout du package village_gaulois et de la classe village. Import de la classe gaulois dans le village.

---
+++ src/village_gaulois/Village.java
@@ -0,0 +1,34 @@
+package village_gaulois;
+
+import personnages.Gaulois;
+
+public class Village {
+
+	private String nom;
+	private int nbVillageois;
+	private Gaulois[] villageois;
+	private Gaulois chef;
+	
+	// faire un constructeur
+	
+	public String getNom() {
+		return nom;
+	}
+	
+	public void getChef() {
+		// gaulois
+	}
+	
+	public void ajouterVillageois(Gaulois gaulois) {
+		
+	}
+	
+	public void trouverVillageois(int numVillageois) {
+		// gaulois
+	}
+	
+	public void afficherVillage() {
+		
+	}
+	
+}

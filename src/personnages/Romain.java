3601d23fb7bbee93d290c2a3ed77d96eee2718c0
Camille Golfin <glc5252a@ensfsi.onmicrosoft.com>
2026-03-17 20:01:54 +0100

    mise en place du système d'équipement pour les romains

--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -1,13 +1,19 @@
 package personnages;
 
+import objets.Equipement;
+
 public class Romain {
 	private String nom;
 	private int force;
+	private Equipement[] equipement;
+	private int nbEquipement;
 
 	public Romain(String nom, int force) {
 		this.nom = nom;
 		this.force = force;
 		assert isInvariantVerified() == true;
+		this.nbEquipement = 0;
+		this.equipement = new Equipement[2];
 	}
 
 	public String getNom() {
@@ -41,5 +47,35 @@ public class Romain {
 	private boolean isInvariantVerified() {
 		return this.force >= 0;
 	}
-
+	
+	public void sEquiper(Equipement equipement) {
+		// cas de figures : inventaire plein, possède déjà l'obj, equip ajouté.
+		
+		// AJOUTER LES OBJETS AU ROMAIN
+		switch(this.nbEquipement) {
+		case 2:
+			System.out.println("Le soldat " + this.nom + " est déjà bien protégé !");
+			break;
+		case 0:
+			System.out.println("Le soldat " + this.nom + " s'équippe avec " + equipement.toString());
+			break;
+		case 1:
+			boolean doublon = false;
+			for (int i = 0; i < this.nbEquipement; i++) {
+				if (equipement = this.equipement[i]) {
+					doublon = true;
+				}
+			}
+			switch(doublon) {
+			case 0:
+				System.out.println("sequippe avec l'item");
+				break;
+			case 1:
+				System.out.println("sequippe avec l'item");
+				break
+			}
+			break;
+		}
+	}
+	
 }
